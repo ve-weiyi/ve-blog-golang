e65e2936efe34c08d4f54c8ae9aa153e5776b8f0
waynie.wei <waynie.wei@anker-in.com>
2023-11-13 19:59:24 +0800

    [其他] 切换下载源

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -10,7 +10,7 @@ cd /home/app
 git clone https://github.com/ve-weiyi/ve-blog-golang.git
 
 # 切换到目标分支并拉取最新代码
-# git switch -c feature/1.0.0/blog
+# git checkout feature/1.0.0/blog
 
 # 切换到项目目录
 cd /home/app/ve-blog-golang/server
@@ -24,3 +24,6 @@ go run main.go server
 
 echo "blog ALL start!!!"
 tail -f /dev/null
+
+# 生成镜像并部署 sudo docker-compose -f docker-compose.yaml up -d
+# 进入镜像查看部署结果 sudo docker exec -it ve-blog-server /bin/bash
