a6480b822e7974f22b44e60d81f887f74d200788
waynie.wei <waynie.wei@anker-in.com>
2023-12-21 18:38:42 +0800

    [优化] swagger解析model优化
    [优化] swagger解析优化
    
    [优化] doc优化
    
    (cherry picked from commit a3e79e2152a761ae6d4851508eba489afc98b38b)

---
+++ server/quickstart/apidocs/apiparser/swagger_test.go
@@ -0,0 +1,45 @@
+package apiparser
+
+import (
+	"testing"
+
+	"github.com/ve-weiyi/ve-blog-golang/server/global"
+	"github.com/ve-weiyi/ve-blog-golang/server/utils/jsonconv"
+)
+
+func TestSwaggerParser(t *testing.T) {
+	ac := NewSwaggerParser()
+	//apis, err := ac.ParseApiDocsByRoots(global.GetRuntimeRoot() + "server/docs")
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//
+	//t.Log(jsonconv.ObjectToJsonIndent(apis))
+
+	models, err := ac.ParseModelDocsByRoots(global.GetRuntimeRoot() + "server/docs")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(jsonconv.ObjectToJsonIndent(models))
+}
+
+func TestAstParser(t *testing.T) {
+	cfg := AstParserConfig{
+		ApiBase: "/api/v1",
+	}
+	ac := NewAstParser(cfg)
+	//apis, err := ac.ParseApiDocsByRoots(global.GetRuntimeRoot() + "server/api/controller")
+	//if err != nil {
+	//	t.Error(err)
+	//}
+	//
+	//t.Log(jsonconv.ObjectToJsonIndent(apis))
+
+	models, err := ac.ParseModelDocsByRoots(global.GetRuntimeRoot() + "server/api/model")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(jsonconv.ObjectToJsonIndent(models))
+}
