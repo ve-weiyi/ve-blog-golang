d7cf67655ad566821a443b5a4ca9035d8e5f083b
waynie.wei <waynie.wei@anker-in.com>
2024-03-27 15:46:10 +0800

    feat: 添加menu接口

--- server/api/model/entity/bs_menu.go
+++ server/api/model/entity/bs_menu.go
@@ -7,17 +7,17 @@ const TableNameMenu = "menu"
 
 // Menu mapped from table <menu>
 type Menu struct {
-	ID        int       `gorm:"column:id;type:int unsigned;primaryKey;autoIncrement:true;comment:主键" json:"id"`                    // 主键
-	Name      string    `gorm:"column:name;type:varchar(32);not null;comment:菜单名" json:"name"`                                     // 菜单名
-	Path      string    `gorm:"column:path;type:varchar(64);not null;comment:菜单路径" json:"path"`                                    // 菜单路径
-	Title     string    `gorm:"column:title;type:varchar(64);not null;comment:菜单标题" json:"title"`                                  // 菜单标题
-	Component string    `gorm:"column:component;type:varchar(64);not null;comment:组件" json:"component"`                            // 组件
-	Icon      string    `gorm:"column:icon;type:varchar(64);not null;comment:菜单icon" json:"icon"`                                  // 菜单icon
-	Rank      int       `gorm:"column:rank;type:tinyint;not null;comment:排序" json:"rank"`                                          // 排序
-	ParentID  int       `gorm:"column:parent_id;type:int;comment:父id" json:"parent_id"`                                            // 父id
-	IsHidden  int       `gorm:"column:is_hidden;type:tinyint(1);not null;comment:是否隐藏  0否1是" json:"is_hidden"`                     // 是否隐藏  0否1是
-	CreatedAt time.Time `gorm:"column:created_at;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:创建时间" json:"created_at"` // 创建时间
-	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:更新时间" json:"updated_at"` // 更新时间
+	ID        int       `gorm:"column:id;type:int unsigned;primaryKey;autoIncrement:true;comment:主键" json:"id"`                             // 主键
+	ParentID  int       `gorm:"column:parent_id;type:int;not null;comment:父id" json:"parent_id"`                                            // 父id
+	Title     string    `gorm:"column:title;type:varchar(32);not null;comment:菜单标题" json:"title"`                                           // 菜单标题
+	Path      string    `gorm:"column:path;type:varchar(64);not null;uniqueIndex:uk_path,priority:1;comment:路由路径" json:"path"`              // 路由路径
+	Name      string    `gorm:"column:name;type:varchar(32);not null;comment:路由名称" json:"name"`                                             // 路由名称
+	Component string    `gorm:"column:component;type:varchar(64);not null;comment:路由组件" json:"component"`                                   // 路由组件
+	Redirect  string    `gorm:"column:redirect;type:varchar(64);not null;comment:路由重定向" json:"redirect"`                                    // 路由重定向
+	Type      int       `gorm:"column:type;type:tinyint unsigned;not null;default:0;comment:菜单类型（0代表菜单、1代表iframe、2代表外链、3代表按钮）" json:"type"` // 菜单类型（0代表菜单、1代表iframe、2代表外链、3代表按钮）
+	Meta      string    `gorm:"column:meta;type:varchar(64);not null;comment:菜单元数据" json:"meta"`                                            // 菜单元数据
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:创建时间" json:"created_at"`          // 创建时间
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:更新时间" json:"updated_at"`          // 更新时间
 }
 
 // TableName Menu's table name
