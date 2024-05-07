070d3c7954f49dbf9f2fc95471aea568d0da66b4
waynie.wei <waynie.wei@anker-in.com>
2024-05-07 11:09:16 +0800

    [优化] 优化

--- server/infra/glog/glog_test.go
+++ server/infra/glog/glog_test.go
@@ -1,11 +1,7 @@
 package glog
 
 import (
-	"context"
 	"testing"
-	"time"
-
-	"github.com/zeromicro/go-zero/core/logx"
 
 	"github.com/ve-weiyi/ve-blog-golang/server/infra/glog/zaplog"
 )
@@ -37,36 +33,4 @@ func BenchmarkExample(b *testing.B) {
 		logs.Sugar().Info("hello", i)
 		logj.Sugar().Info("hello", i)
 	}
-}
-
-// https://zhuanlan.zhihu.com/p/652570748
-func TestGoZero(t *testing.T) {
-	c := logx.LogConf{
-		ServiceName:         "12121",
-		Mode:                "file",
-		Encoding:            "json",
-		TimeFormat:          time.RFC3339,
-		Path:                "./logs",
-		Level:               "debug",
-		MaxContentLength:    0,
-		Compress:            false,
-		Stat:                false,
-		KeepDays:            10,
-		StackCooldownMillis: 0,
-		MaxBackups:          0,
-		MaxSize:             0,
-		Rotation:            "",
-	}
-	logx.SetUp(c)
-
-	ctx := logx.ContextWithFields(context.Background(), logx.Field("trace", "test"))
-
-	logx.WithContext(ctx).Info("hello world")
-	logx.WithContext(ctx).Info("hello world")
-	logx.WithContext(ctx).Info("hello world")
-	logx.WithContext(ctx).Info("hello world")
-	logx.WithContext(ctx).Debug("hello world")
-	logx.WithContext(ctx).Slow("hello world")
-	logx.WithContext(ctx).Error("hello world")
-	logx.Close()
 }
--- zero/service/blog/api/proto/generate.sh
+++ zero/service/blog/api/proto/generate.sh
@@ -1,1 +1,1 @@
-goctl api go -api blog.api -dir ../ --style go_zero --home ../../../../script/template
+goctl api go -api blog.api -dir ../ --style go_zero --home ../../../../resource/template
