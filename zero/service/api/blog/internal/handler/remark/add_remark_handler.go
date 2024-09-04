6755690e7397ba13530c8d9fa2366071f88176f1
waynie.wei <waynie.wei@anker-in.com>
2024-09-04 18:45:20 +0800

    feat: remark 优化
    (cherry picked from commit c86edef50a123e38b1057a8b690b58a25e861e74)

--- zero/service/api/blog/internal/handler/remark/add_remark_handler.go
+++ zero/service/api/blog/internal/handler/remark/add_remark_handler.go
@@ -14,7 +14,7 @@ import (
 // 创建留言
 func AddRemarkHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var req types.Remark
+		var req types.RemarkNewReq
 		if err := httpx.Parse(r, &req); err != nil {
 			responsex.Response(r, w, nil, err)
 			return
