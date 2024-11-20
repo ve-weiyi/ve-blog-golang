593addeb3447ee18e8f77b106a7c0f936026a5b3
waynie.wei <waynie.wei@anker-in.com>
2024-11-20 18:58:58 +0800

    feat: 优化oss文件上传

---
+++ kit/infra/oss/oss.go
@@ -0,0 +1,30 @@
+package oss
+
+import (
+	"io"
+	"mime/multipart"
+)
+
+type Config struct {
+	Zone            string `json:"zone"`
+	Endpoint        string `json:"endpoint"`
+	AccessKeyId     string `json:"access-key-id"`
+	AccessKeySecret string `json:"access-key-secret"`
+	BucketName      string `json:"bucket-name"`
+	BucketUrl       string `json:"bucket-url"`
+	BasePath        string `json:"base-path"`
+}
+
+// OSS（Object Storage Service，对象存储服务）
+type OSS interface {
+	// return BucketUrl+BasePath+prefix+filename
+	UploadHttpFile(file *multipart.FileHeader, prefix string, filename string) (url string, err error)
+	UploadLocalFile(filepath string, prefix string, filename string) (url string, err error)
+	DeleteFile(key string) error
+	ListFiles(prefix string, limit int) (keys []string, err error)
+}
+
+type File interface {
+	Open() (File, error)
+	io.Writer
+}
